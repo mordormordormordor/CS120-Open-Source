e0126bee28dd17f1b4e31c59719493edad2c2938
JVT135 <57076677+JVT135@users.noreply.github.com>
2023-03-21 16:43:16 -0400

    Update even_odd_site.py
    fixed comments

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,11 +1,11 @@
-// Open HTML file 
+# Open HTML file 
 with open("numbers.html", "w") as f:
     
-    // Write to HTML file
+    # Write to HTML file
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     
-    // Even-Odd calc and write to HTML
+    # Even-Odd calc and write to HTML
     for i in range(1, 50):
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
@@ -13,7 +13,7 @@ with open("numbers.html", "w") as f:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
 
-// Write HTML file to console
+# Write HTML file to console
 with open("numbers.html", "r") as f:
     print(f.read())
     
