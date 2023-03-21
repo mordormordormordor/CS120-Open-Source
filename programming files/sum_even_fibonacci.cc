065ec1e6a5d15fefdd994a580f52223cc427c2b3
Eric <67992282+mordormordormordor@users.noreply.github.com>
2023-03-21 16:45:15 -0400

    Update sum_even_fibonacci.cc
    Adding comments

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -4,16 +4,19 @@ int main()
 {
     int a = 0, b = 1;
     int sum_even = 0;
+    //Setting limit
     while (b < 4000000) {
+        //Sum all even in the sequence
         if (b % 2 == 0) {
             sum_even += b;
         }
         int temp = b;
+        //Resetting b so next loop a is b and b is the last number we calculated
         b = a + b;
         a = temp;
     }
+    //Print to screen
     std::cout << sum_even << std::endl;
-
     return 0;
 }
 
