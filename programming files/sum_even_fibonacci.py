f4a6565723661aeed51c062549267c57b633e8b3
Eric <67992282+mordormordormordor@users.noreply.github.com>
2023-03-21 16:43:45 -0400

    Update sum_even_fibonacci.py
    Added comments

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,14 @@
 a, b = 0, 1
 sum_even = 0
+#Setting limit for b
 while b < 4000000:
+    #If b is even add it to sum_even
     if b % 2 == 0:
         sum_even += b
+    #Set a = b and b = a+b
     a, b = b, a+b
 
+#Print out the total of all the even numbers
 print(sum_even)
 
 # answer should be 4613732
