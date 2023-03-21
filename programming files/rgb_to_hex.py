599f83546ae47c097ea6e711d3d6f71e6a22d72b
Brandon Wolf <112419054+BMWolf57@users.noreply.github.com>
2023-03-21 16:43:07 -0400

    Update rgb_to_hex.py
    added comments to rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,7 +1,9 @@
 def rgb_to_hex(r, g, b):
+    #creates rgb values and then turns them into hexadecimal numbers
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
+    #returns a hexadecimal string
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
